74a4f190f383360381b10b91731efcafbd1fd9af
Fabio <fabio.silva910@etec.sp.gov.br>
2024-04-02 22:16:50 -0300

    fabio2

---
+++ src/Components/Title/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {View, Text} from "react-native-web";
+
+export default function Title()
+{
+    return(
+        <View>
+            <Text>Meu primeiro aplicativo</Text>
+        </View>
+    );
+}
